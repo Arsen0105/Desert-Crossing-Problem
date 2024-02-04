12de690cb171f8dbb2104079c56effe11ffcd9f5
Arsen0105 <118631903+Arsen0105@users.noreply.github.com>
2024-02-04 16:13:06 +0700

    Update test_2.py

--- test_2.py
+++ test_2.py
@@ -2,13 +2,16 @@ s = 1000
 l = 1
 v = 500
 
+# Дальность поездки наодном бензобаке
 max_car = v / l
 
+# Количество бензобаков
 n = 1
-bank = 0
-while bank + max_car < s:
-    bank += 1 / (1 + 2 * n) * max_car
+# Задача решается от конца, поэтому суммируем расстояние, которое проедем расставив бензобаки в будущем 
+stock = 0
+while stock + max_car < s:
+    stock += 1 / (1 + 2 * n) * max_car
     n += 1
-    # print(n, bank, sep='\t\t')
+    # print(n, stock, sep='\t\t')
 
 print(n)
